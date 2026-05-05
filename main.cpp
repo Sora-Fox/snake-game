1654115f4878be40d12082e40e5e2551fecf2ea2
Sora-fox <106887689+Sora-Fox@users.noreply.github.com>
2026-05-05 18:02:15 +0300

    Add xlib impl

--- main.cpp
+++ main.cpp
@@ -1,144 +1,280 @@
-#include <algorithm>
-#include <cstdint>
-#include <cstdlib>
-#include <exception>
+#include <chrono>
+#include <cstdio>
 #include <print>
-#include <random>
 #include <stdexcept>
-#include <string>
-#include <vector>
-#include <ncurses.h>
+#include <thread>
+
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+
 #include "game_model.hpp"
 
 namespace snake {
-  struct ncurses_guard;
-  enum class input_result : std::uint8_t {
-    success,
-    exit,
+  class x11_gui final {
+  public:
+    x11_gui();
+    ~x11_gui();
+
+    void run();
+
+  private:
+    Display* const display_ = nullptr;
+    Screen* const screen_ = nullptr;
+    Window win_{};
+    GC gc_{};
+    game_model model_;
+    int tile_size_ = 0;
+    int x_offset_ = 0;
+    int y_offset_ = 0;
+    bool is_running_ = false;
+
+    void update_tile_size_and_offset();
+    void close_connection() noexcept;
+    void perform_step();
+
+    void handle_event(XEvent&);
+    void handle_expose(const XEvent&);
+    void handle_keypress(XEvent&);
+
+    void draw_tile(int x, int y, unsigned long color);
+    void draw_bg_tile(int x, int y);
+    void draw_snake_head_tile(int x, int y);
+    void draw_snake_body_tile(int x, int y);
+    void draw_food_tile(int x, int y);
+
+    void draw_bg();
+    void draw_snake();
+    void draw_food();
+    void draw_border();
+    void draw_frame();
   };
-  [[nodiscard]] input_result parse_input(game_model&);
-  void draw_snake(WINDOW*, const std::vector<point_t>&);
-  void draw_food(WINDOW*, point_t);
-  void draw_scores(WINDOW*, std::size_t);
-  void draw_frame(WINDOW*, const game_model&);
-  void draw_game_over(WINDOW*, const game_model&);
-  void print_center(WINDOW*, std::string_view, int);
 }
 
-struct snake::ncurses_guard {
-  static constexpr auto col_yellow = 1;
-  static constexpr auto col_green = 2;
-  static constexpr auto col_red = 3;
+namespace {
+  Display* open_display();
+  Window create_window(Display*, Screen*);
+  Screen* get_screen(Display*);
+}
 
-  ncurses_guard();
-  ~ncurses_guard();
-};
+snake::x11_gui::x11_gui() :
+  display_(open_display()),
+  screen_(get_screen(display_)),
+  win_(create_window(display_, screen_)),
+  gc_(XCreateGC(display_, win_, 0, nullptr)),
+  model_(32, 32),
+  is_running_(false) {
+  XSelectInput(display_, win_, ExposureMask | KeyPressMask);
+  XMapWindow(display_, win_);
+  update_tile_size_and_offset();
+  std::println(stderr, "Vendor  {}", XServerVendor(display_));
+  std::println(stderr, "Release {}", XVendorRelease(display_));
+  std::println(stderr, "Display {}", XDisplayString(display_));
+}
 
-snake::ncurses_guard::ncurses_guard() {
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, true);
-  curs_set(0);
-  timeout(150);
-  if (!has_colors()) {
-    endwin();
-    throw std::runtime_error("Colors are not supported");
+snake::x11_gui::~x11_gui() {
+  close_connection();
+}
+
+void snake::x11_gui::close_connection() noexcept {
+  XFreeGC(display_, gc_);
+  XDestroyWindow(display_, win_);
+  XCloseDisplay(display_);
+  std::println(stderr, "X connection was closed");
+}
+
+void snake::x11_gui::run() {
+  is_running_ = true;
+  draw_frame();
+  auto event = XEvent{};
+  while (is_running_) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    while (XPending(display_)) {
+      XNextEvent(display_, &event);
+      handle_event(event);
+    }
+    perform_step();
   }
-  start_color();
-  init_pair(col_yellow, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(col_green, COLOR_GREEN, COLOR_BLACK);
-  init_pair(col_red, COLOR_RED, COLOR_BLACK);
 }
 
-snake::ncurses_guard::~ncurses_guard() {
-  endwin();
+void snake::x11_gui::perform_step() {
+  const auto old_food = model_.get_food();
+  const auto old_tail = model_.get_snake().back();
+  const auto old_head = model_.get_snake().front();
+  model_.step();
+  const auto new_head = model_.get_snake().front();
+  if (new_head == old_food) {
+    const auto new_food = model_.get_food();
+    draw_food_tile(new_food.x, new_food.y);
+  } else {
+    draw_bg_tile(old_tail.x, old_tail.y);
+  }
+  draw_snake_body_tile(old_head.x, old_head.y);
+  draw_snake_head_tile(new_head.x, new_head.y);
 }
 
-void snake::draw_snake(WINDOW* win, const std::vector<point_t>& points) {
-  const static auto printer = [win](auto p) { mvwprintw(win, p.y, p.x * 2, "%s", "[]"); };
-  wattron(win, COLOR_PAIR(ncurses_guard::col_yellow));
-  printer(points.front());
-  wattroff(win, COLOR_PAIR(ncurses_guard::col_yellow));
-  wattron(win, COLOR_PAIR(ncurses_guard::col_green));
-  std::for_each(points.begin() + 1, points.end(), printer);
-  wattroff(win, COLOR_PAIR(ncurses_guard::col_green));
-}
-
-void snake::draw_food(WINDOW* win, const point_t food) {
-  wattron(win, COLOR_PAIR(ncurses_guard::col_red));
-  mvwprintw(win, food.y, food.x * 2, "%s", "[]");
-  wattroff(win, COLOR_PAIR(ncurses_guard::col_red));
-}
-
-void snake::draw_scores(WINDOW* win, const std::size_t scores) {
-  const auto max_y = getmaxy(win);
-  print_center(win, "Scores: " + std::to_string(scores), max_y - 1);
-}
-
-void snake::draw_frame(WINDOW* win, const game_model& model) {
-  werase(win);
-  box(win, 0, 0);
-  draw_snake(win, model.get_snake());
-  draw_food(win, model.get_food());
-  draw_scores(win, model.get_scores());
-  wrefresh(win);
-}
-
-void snake::draw_game_over(WINDOW* win, const game_model& model) {
-  const auto scores_msg = "Scores: " + std::to_string(model.get_scores());
-  const auto mid_y = getmaxy(win) / 2;
-  print_center(win, "Game Over", mid_y - 1);
-  print_center(win, scores_msg, mid_y);
-  print_center(win, "Press q to exit", mid_y + 1);
-  wrefresh(win);
-}
-
-void snake::print_center(WINDOW* win, const std::string_view str, const int y) {
-  const auto x = (getmaxx(win) - str.size()) / 2;
-  mvwaddnstr(win, y, x, str.data(), str.size());
-}
-
-snake::input_result snake::parse_input(game_model& model) {
-  switch (getch()) {
-  case KEY_UP:
-  case 'k':
-    model.set_direction(snake::direction::up);
+void snake::x11_gui::handle_event(XEvent& event) {
+  switch (event.type) {
+  case Expose:
+    handle_expose(event);
     break;
-  case KEY_DOWN:
-  case 'j':
-    model.set_direction(snake::direction::down);
+  case KeyPress:
+    handle_keypress(event);
     break;
-  case KEY_RIGHT:
-  case 'l':
-    model.set_direction(snake::direction::right);
-    break;
-  case KEY_LEFT:
-  case 'h':
-    model.set_direction(snake::direction::left);
-    break;
+  }
+}
+
+void snake::x11_gui::handle_expose(const XEvent& event) {
+  if (event.xexpose.count != 0) {
+    return;
+  }
+  update_tile_size_and_offset();
+  std::println(stderr, "Expose");
+#if 0
+  XSetForeground(display_, gc_, 0xFF0000);
+  for (int x = 0; x != model_.cols(); ++x) {
+    for (int y = 0; y != model_.rows(); ++y) {
+      unsigned long color = 0x888888;
+      if (x % 2 == y % 2) {
+        color = 0x444444;
+      }
+      draw_tile(x, y, color);
+    }
+  }
+#endif
+  draw_frame();
+}
+
+void snake::x11_gui::handle_keypress(XEvent& event) {
+  const auto keysym = XLookupKeysym(&event.xkey, 0);
+  std::println(stderr, "KeyPress [keysym={}]", keysym);
+  switch (keysym) {
   case 'q':
-    return input_result::exit;
+    is_running_ = false;
+    break;
+  case 'k':
+  case XK_Up:
+    model_.set_direction(direction::up);
+    break;
+  case 'j':
+  case XK_Down:
+    model_.set_direction(direction::down);
+    break;
+  case 'h':
+  case XK_Left:
+    model_.set_direction(direction::left);
+    break;
+  case 'l':
+  case XK_Right:
+    model_.set_direction(direction::right);
+    break;
   }
-  return input_result::success;
+}
+
+void snake::x11_gui::update_tile_size_and_offset() {
+  auto [h, w] = std::pair(model_.rows(), model_.cols());
+  auto attrs = XWindowAttributes{};
+  XGetWindowAttributes(display_, win_, &attrs);
+  tile_size_ = std::min(attrs.width / (w + 1), attrs.height / (h + 1));
+  x_offset_ = (attrs.width - 1 - tile_size_ * w) / 2;
+  y_offset_ = (attrs.height - 1 - tile_size_ * h) / 2;
+}
+
+void snake::x11_gui::draw_tile(int x, int y, unsigned long color) {
+  const auto x_coord = x * tile_size_ + x_offset_;
+  const auto y_coord = y * tile_size_ + y_offset_;
+  XSetForeground(display_, gc_, color);
+  XFillRectangle(display_, win_, gc_, x_coord, y_coord, tile_size_, tile_size_);
+}
+
+void snake::x11_gui::draw_bg_tile(int x, int y) {
+  const auto color = XBlackPixelOfScreen(screen_);
+  draw_tile(x, y, color);
+}
+
+void snake::x11_gui::draw_snake_head_tile(int x, int y) {
+  const auto color = 0xFFFF00;
+  draw_tile(x, y, color);
+}
+
+void snake::x11_gui::draw_snake_body_tile(int x, int y) {
+  const auto color = 0x00FF00;
+  draw_tile(x, y, color);
+}
+
+void snake::x11_gui::draw_food_tile(int x, int y) {
+  const auto color = 0xFF0000;
+  draw_tile(x, y, color);
+}
+
+void snake::x11_gui::draw_bg() {
+  const auto color = XBlackPixelOfScreen(screen_);
+  auto attrs = XWindowAttributes{};
+  XGetWindowAttributes(display_, win_, &attrs);
+  XSetForeground(display_, gc_, color);
+  XFillRectangle(display_, win_, gc_, 0, 0, attrs.width, attrs.height);
+}
+
+void snake::x11_gui::draw_snake() {
+  const auto& snake = model_.get_snake();
+  const auto head = snake.front();
+  draw_snake_head_tile(head.x, head.y);
+  for (auto i = snake.begin() + 1; i != snake.end(); ++i) {
+    const auto& p = *i;
+    draw_snake_body_tile(p.x, p.y);
+  }
+}
+
+void snake::x11_gui::draw_food() {
+  const auto food = model_.get_food();
+  draw_food_tile(food.x, food.y);
+}
+
+void snake::x11_gui::draw_border() {
+  const auto white = XWhitePixelOfScreen(screen_);
+  XSetForeground(display_, gc_, white);
+  XDrawRectangle(display_, win_, gc_, x_offset_ - 1, y_offset_ - 1,
+      model_.cols() * tile_size_ + 1, model_.rows() * tile_size_ + 1);
+}
+
+void snake::x11_gui::draw_frame() {
+  draw_bg();
+  draw_snake();
+  draw_food();
+  draw_border();
+}
+
+namespace {
+  Display* open_display() {
+    Display* display = XOpenDisplay(nullptr);
+    if (!display) {
+      throw std::runtime_error("XOpenDisplay faild");
+    }
+    return display;
+  }
+
+  Window create_window(Display* display, Screen* screen) {
+    const auto black = XBlackPixelOfScreen(screen);
+    const auto root = XRootWindowOfScreen(screen);
+    constexpr static auto h = 740;
+    constexpr static auto w = 1020;
+    return XCreateSimpleWindow(display, root, 0, 0, w, h, 0, black, black);
+  }
+
+  Screen* get_screen(Display* display) {
+    const auto screen_number = XDefaultScreen(display);
+    Screen* screen = XScreenOfDisplay(display, screen_number);
+    if (!screen) {
+      XCloseDisplay(display);
+      throw std::runtime_error("XScreenOfDisplay faild");
+    }
+    return screen;
+  }
+
 }
 
 int main() try {
-  const auto guard = snake::ncurses_guard{};
-  const auto height = std::min(LINES, 22);
-  const auto width = std::min(COLS, 42);
-  const auto win = newwin(height, width, 0, 0);
-  auto model = snake::game_model(height, width / 2);
-  auto is_running = true;
-  while (model.step() && is_running) {
-    snake::draw_frame(win, model);
-    is_running = snake::parse_input(model) != snake::input_result::exit;
-  }
-  snake::draw_game_over(win, model);
-  auto ch = getch();
-  while (ch != 'q' && ch != 'Q') {
-    ch = getch();
-  }
+  snake::x11_gui gui;
+  gui.run();
 } catch (const std::exception& e) {
   std::println(stderr, "Error: {}", e.what());
   return 1;
